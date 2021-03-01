4d10fb696c7636302280f0d5d1097f4b6945d3ca
qhj <1550277807@qq.com>
2021-03-01 13:09:06 +0800

    修改文件目录

---
+++ consumer/src/main/java/com/ccw/lease/consumer/controller/test.java
@@ -0,0 +1,14 @@
+package com.ccw.lease.consumer.controller;
+
+/**
+ * @version 1.0
+ * @projectName:com.ccw.lease.consumer.controller
+ * @packageName:lease
+ * @className:test
+ * @Description:[一句话描述该类的功能]
+ * @Author:qhj
+ * @CreateDate:2021/3/1 13:08
+ * @blog:[方法具体上行/下行参数文档]
+ */
+public class test {
+}
---
+++ provider/src/main/java/com/ccw/lease/provicer/dao/test.java
@@ -0,0 +1,14 @@
+package com.ccw.lease.provicer.dao;
+
+/**
+ * @version 1.0
+ * @projectName:com.ccw.lease.provicer.dao
+ * @packageName:lease
+ * @className:test
+ * @Description:[一句话描述该类的功能]
+ * @Author:qhj
+ * @CreateDate:2021/3/1 13:07
+ * @blog:[方法具体上行/下行参数文档]
+ */
+public class test {
+}
